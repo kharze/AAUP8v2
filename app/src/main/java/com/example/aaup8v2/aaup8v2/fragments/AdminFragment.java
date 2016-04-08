83387b3a14d0c54835fc9444156fe2716a84b76a
kharze <mgrege11@student.aau.dk>
2016-04-08 09:26:52 +0200

    fixed playbar to bottom

--- app/src/main/java/com/example/aaup8v2/aaup8v2/fragments/AdminFragment.java
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/fragments/AdminFragment.java
@@ -7,48 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 import com.example.aaup8v2.aaup8v2.R;
+import com.example.aaup8v2.aaup8v2.asyncTasks.asyncGetPlaylistTracks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistTrack;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AdminFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AdminFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdminFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    Spinner spinner;
+    List<HashMap<String,String>> aList = new ArrayList<>();
     private OnFragmentInteractionListener mListener;
 
     public AdminFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AdminFragment newInstance(String param1, String param2) {
         AdminFragment fragment = new AdminFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,17 +43,50 @@ public class AdminFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false);
+
+
+
+
+
+        new asyncGetPlaylistTracks(new asyncGetPlaylistTracks.AsyncResponse(){
+
+            @Override
+            public void processFinish(Pager output){
+
+                for(int i=0;i < output.items.size();i++){
+                    HashMap<String, String> hm = new HashMap<>();
+
+                    PlaylistTrack p = (PlaylistTrack) output.items.get(i);
+                    String s = p.track.name;
+                    hm.put("txt", s);
+                    hm.put("cur", "Artist : " + p.track.artists.get(0).name);
+                    aList.add(hm);
+                }
+
+                String[] from = { "flag","txt","cur" };
+
+                int[] to = { R.id.flag,R.id.txt,R.id.cur,R.id.textView};
+                SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_layout,from,to );
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+               // Apply the adapter to the spinner
+                spinner.setAdapter(adapter);
+            }
+        }).execute("spotify_denmark", "2qPIOBAKYc1SQI1QHDV4EV");
+
+
+
+        View v = inflater.inflate(R.layout.fragment_admin, container,false);
+        spinner = (Spinner) v.findViewById(R.id.playlist_spinner);
+
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
