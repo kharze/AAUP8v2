f3b73835e38f3e37888f8f7189a78d90db3cbbb9
Claus Madsen <claus.nygaard.madsen@gmail.com>
2016-04-11 13:19:45 +0200

    Changed asyncTasks for getPlaylists and getTracks to handle more than 100 elements

--- app/src/main/java/com/example/aaup8v2/aaup8v2/asyncTasks/asyncGetTracks.java
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/asyncTasks/asyncGetTracks.java
@@ -4,6 +4,10 @@ import android.os.AsyncTask;
 
 import com.example.aaup8v2.aaup8v2.MainActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.Tracks;
 
 public class asyncGetTracks extends AsyncTask<String, Void, Tracks> {
@@ -25,7 +29,32 @@ public class asyncGetTracks extends AsyncTask<String, Void, Tracks> {
 
     @Override
     protected Tracks doInBackground(String... id) {
-        return MainActivity.mSpotifyAccess.mService.getTracks(id[0]);
+        try {
+            Tracks tracks = null;
+            int offset = 0;
+            Tracks temp;
+
+            do{
+                Map<String, Object> options = new HashMap<>();
+                options.put(MainActivity.mSpotifyAccess.mService.OFFSET, offset);
+                temp = MainActivity.mSpotifyAccess.mService.getTracks(id[0], options);
+                if (tracks == null){
+                    tracks = temp;
+                }
+                else {
+                    for(int i = 0; i < temp.tracks.size(); i++){
+                        tracks.tracks.add(temp.tracks.get(i));
+                    }
+                }
+                offset += 100;
+            }while ((tracks.tracks.size() % 100) == 0 && temp.tracks.size() != 0);
+            return tracks;
+        } catch (Exception e) {
+            e.getCause();
+            return null;
+        }
+
+        //return MainActivity.mSpotifyAccess.mService.getTracks(id[0]);
     }
 
     @Override
