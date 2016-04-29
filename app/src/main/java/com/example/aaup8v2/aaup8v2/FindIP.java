99e0fe376c5de6b813079959cacd6ee6ce2f2f0b
zatiru <mladen10@student.aau.dk>
2016-04-29 13:55:14 +0200

    FindIP class added - again!!

---
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/FindIP.java
@@ -0,0 +1,40 @@
+package com.example.aaup8v2.aaup8v2;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+/**
+ * Created by zatiru on 29-04-2016.
+ */
+public class FindIP {
+    /**
+     * Get IP address from first non-localhost interface
+     * @param, ipv4  true=return ipv4, false=return ipv6
+     * @return  address or empty string
+     */
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+}
