30eb7d46cf91cd0813c639d01c8b68ad7a6c7e16
sthem12@student.aau.dk <pjuskr@gmail.com>
2016-05-06 21:58:21 +0200

    Made some few improvements to SearchMusicRunnable

--- app/src/main/java/com/example/aaup8v2/aaup8v2/Runnables/GetAlbumsRunnable.java
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/Runnables/GetAlbumsRunnable.java
@@ -2,21 +2,43 @@ package com.example.aaup8v2.aaup8v2.Runnables;
 
 import com.example.aaup8v2.aaup8v2.MainActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kaaes.spotify.webapi.android.models.Albums;
 
 /**
  * Created by Sean Skov Them on 04-05-2016.
  */
 public class GetAlbumsRunnable extends ThreadResponseInterface<Albums> implements Runnable {
-    private String artistId;
+    private List<String> artistIds;
 
-    public GetAlbumsRunnable(String artistId, ThreadResponse<Albums> delegate) {
-        this.artistId = artistId;
+    public GetAlbumsRunnable(List<String> artistIds, ThreadResponse<Albums> delegate) {
+        this.artistIds = artistIds;
         this.delegate = delegate;
     }
 
     public void run() {
-        try { delegate.processFinish(MainActivity.mSpotifyAccess.mService.getAlbums(artistId)); }
-        catch (Exception e) { delegate.processFinish(new Albums()); }
+        Albums albums = new Albums();
+        albums.albums = new ArrayList<>();
+        do {
+            String albumIds = null;
+            int counter = 0;
+            do {
+                if (albumIds == null) {
+                    albumIds = artistIds.get(0);
+                    artistIds.remove(0);
+                } else {
+                    albumIds += "," + artistIds.get(0);
+                    artistIds.remove(0);
+                }
+                counter++;
+            } while (counter < 50 && !artistIds.isEmpty());
+
+            albums.albums.addAll(MainActivity.mSpotifyAccess.mService.getAlbums(albumIds).albums);
+        } while (!artistIds.isEmpty());
+
+        delegate.processFinish(albums);
+
     }
 }
--- app/src/main/java/com/example/aaup8v2/aaup8v2/Runnables/SearchTracksRunnable.java
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/Runnables/SearchTracksRunnable.java
@@ -2,6 +2,10 @@ package com.example.aaup8v2.aaup8v2.Runnables;
 
 import com.example.aaup8v2.aaup8v2.MainActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.TracksPager;
 
 /**
@@ -9,15 +13,36 @@ import kaaes.spotify.webapi.android.models.TracksPager;
  */
 public class SearchTracksRunnable extends ThreadResponseInterface<TracksPager> implements Runnable {
     String searchTerm;
+    private int resultLimit;
 
-    public SearchTracksRunnable(String searchTerm, ThreadResponse<TracksPager> delegate){
+    public SearchTracksRunnable(String searchTerm, int resultLimit, ThreadResponse<TracksPager> delegate){
         this.searchTerm = searchTerm;
+        this.resultLimit = resultLimit;
         this.delegate = delegate;
     }
 
     @Override
     public void run() {
-        try { delegate.processFinish(MainActivity.mSpotifyAccess.mService.searchTracks(searchTerm)); }
-        catch (Exception e){ delegate.processFinish(new TracksPager()); }
+
+        Map<String, Object> options = new HashMap<>();
+        int offset = 0;
+        int limit = 50;
+        TracksPager results = null;
+        TracksPager temp;
+
+        do{
+            options.put(SpotifyService.OFFSET, offset);
+            options.put(SpotifyService.LIMIT, limit);
+            temp = MainActivity.mSpotifyAccess.mService.searchTracks(searchTerm, options);
+            if(results == null){
+                results = temp;
+            } else {
+                results.tracks.items.addAll(temp.tracks.items);
+            }
+            offset += 50;
+
+        }while(!temp.tracks.items.isEmpty() && offset < resultLimit);
+
+        delegate.processFinish(results);
     }
 }
