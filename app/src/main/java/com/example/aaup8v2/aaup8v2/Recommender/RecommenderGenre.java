19a7dc5ff8382f01864eb394ae821a2f69cce074
Lasse Drustrup Christensen <Lasse.d.c91@gmail.com>
2016-05-25 15:19:44 +0200

    Small corrections

--- app/src/main/java/com/example/aaup8v2/aaup8v2/Recommender/RecommenderGenre.java
+++ app/src/main/java/com/example/aaup8v2/aaup8v2/Recommender/RecommenderGenre.java
@@ -4,12 +4,10 @@ package com.example.aaup8v2.aaup8v2.Recommender;
  * Created by lasse on 07-04-2016.
  */
 public class RecommenderGenre {
-    public Integer id;
     public String genre;
     public int count;
 
-    public RecommenderGenre(Integer id, String genre, int count){
-        this.id = id;
+    public RecommenderGenre(String genre, int count){
         this.genre = genre;
         this.count = count;
     }
